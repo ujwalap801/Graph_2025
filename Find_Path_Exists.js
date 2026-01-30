36b6acbec0cf043779fee6730d23d7ca6941ae69
Ujwala <ujwalap801@gmail.com>
2026-01-30 08:55:59 +0530

    Added DFS Solution for Find_Path_Exists

--- Find_Path_Exists.js
+++ Find_Path_Exists.js
@@ -1,10 +1,4 @@
-/**
- * @param {number} n
- * @param {number[][]} edges
- * @param {number} source
- * @param {number} destination
- * @return {boolean}
- */
+// BFS 
 var validPath = function(n, edges, source, destination) {
     let map ={};
 
@@ -43,4 +37,116 @@ var validPath = function(n, edges, source, destination) {
     }
 
     return false;
+};
+
+
+
+
+
+// DFS
+
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+    let map ={};
+
+    for(let [x, y] of edges)
+    {
+
+        if(!map[x]) map[x] =[];
+        if(!map[y]) map[y] =[];
+        map[x].push(y);
+        map[y].push(x);
+    }
+
+    // console.log(map);
+
+    let stack =[source];
+
+    let visited = new Set();
+    visited.add(source);
+
+    while(stack.length)
+    {
+    let curr = stack.pop();
+    if(curr ==destination )
+    {
+        return true;
+    }
+   for(let neighbor of map[curr])
+   {
+   if(!visited.has(neighbor))
+   {
+    stack.push(neighbor);
+    visited.add(neighbor);
+   }
+   }
+
+    }
+
+    return false;
+};
+
+
+
+
+// DFS RECRUSIVE
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+    let map ={};
+
+    for(let [x,y] of edges)
+    {
+        if(!map[x])  map[x] =[];
+
+        if(!map[y]) map[y] =[];
+
+        map[x].push(y);
+        map[y].push(x);
+
+
+    }
+
+    // console.log(map);
+
+    let visited = new Set();
+
+    let dfs =(curr)=>
+    {
+     
+     if(curr ===destination )
+     {
+        return true;
+     }
+     for(let neighbor of map[curr])
+     {
+        if(!visited.has(neighbor))
+        {
+            visited.add(neighbor);
+           if(dfs(neighbor))
+           {
+            return true;
+           }
+        }
+     }
+
+
+     return false;
+    }
+
+
+    return dfs(source)
+
 };
